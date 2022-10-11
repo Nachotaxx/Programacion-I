4c490c819dc20c307bfe1d813fd4da36cfd6f0ae
Nachotaxx <115548697+Nachotaxx@users.noreply.github.com>
2022-10-11 09:33:27 -0300

    Add files via upload

---
+++ PROG – 27.py
@@ -0,0 +1,16 @@
+# Autor: Ignacio Santos
+# Fecha: 11/10/22
+#
+#
+"""
+Escribí un programa donde se declare una lista con todas las materias que estás cursado en la facultad. A continuación,
+programa un bucle para listar por pantalla todas las materias.
+"""
+#
+
+materias = ["Programacion I", "Fisica II", "Practica Profesional I", "Analisis II", "Algrbra II"]
+
+# Bucle que liste x pantalla las materias
+for materias in materias:
+    print(materias)
+#
